77c32280c22e2a881e10937f9480a2a78e6d7337
rafaelldi <rival.abdrakhmanov@protonmail.com>
2019-03-09 22:09:54 +0300

    добавлена CORS политика

--- src/TodoList.Web/Models/ToDoItem.cs
+++ src/TodoList.Web/Models/ToDoItem.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoList.Web.Models
 {
     public class ToDoItem
     {
         public int? Id { get; set; }
+        [Required]
         public string Content { get; set; }
     }
 }
